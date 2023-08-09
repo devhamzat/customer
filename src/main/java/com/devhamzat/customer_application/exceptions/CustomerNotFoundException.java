9a4f05ab8c9f3c7fd710b381eb38f28731bf61c2
devhamzat <ayomidelegbede@gmail.com>
2023-08-09 13:02:03 +0100

    i removed the security

--- src/main/java/com/devhamzat/customer_application/exceptions/CustomerNotFoundException.java
+++ src/main/java/com/devhamzat/customer_application/exceptions/CustomerNotFoundException.java
@@ -2,6 +2,7 @@ package com.devhamzat.customer_application.exceptions;
 
 public class CustomerNotFoundException extends RuntimeException {
     public CustomerNotFoundException(String error) {
+
         super(error);
     }
 }
--- src/main/java/com/devhamzat/customer_application/service/CustomerService.java
+++ src/main/java/com/devhamzat/customer_application/service/CustomerService.java
@@ -4,10 +4,7 @@ import com.devhamzat.customer_application.dao.CustomerDAO;
 import com.devhamzat.customer_application.exceptions.CustomerNotFoundException;
 import com.devhamzat.customer_application.model.Customer;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Component;
-
-import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.CopyOnWriteArrayList;
@@ -18,33 +15,19 @@ public class CustomerService {
     @Autowired
     private CustomerDAO customerDAO;
 
-//    private int customerIdCount = 1;
     private List<Customer> customerList = new CopyOnWriteArrayList<>();
-
     public Customer addCustomer(Customer customer) {
-//        customer.setCustomerId(customerIdCount);
-//        customerList.add(customer);
-//        customerIdCount++;
         return customerDAO.save(customer);
-//        return customer;
-
     }
 
     public List<Customer> getCustomer() {
-
         return customerDAO.findAll();
-//        return customerList;
+
     }
 
     public Customer getCustomer(int customerId) {
-//      return   customerList
-//                .stream()
-//                .filter(c -> c.getCustomerId()==customerId)
-//                .findFirst()
-////                .get();
-//return customerDAO.findAll
-        Optional<Customer>optionalCustomer= customerDAO.findById(customerId);
-        if (!optionalCustomer.isPresent()){
+        Optional<Customer> optionalCustomer = customerDAO.findById(customerId);
+        if (!optionalCustomer.isPresent()) {
             throw new CustomerNotFoundException("Customer record is not available");
         }
         return optionalCustomer.get();
@@ -52,31 +35,10 @@ public class CustomerService {
 
     public Customer updateCustomer(int customerId, Customer customer) {
         customer.setCustomerId(customerId);
-//        customerList
-//                .stream()
-//                .forEach(c->{
-//                    if (c.getCustomerId()==customerId){
-//                        c.setCustomerFirstName(customer.getCustomerFirstName());
-//                        c.setCustomerLastName(customer.getCustomerLastName());
-//                        c.setCustomerEmail(customer.getCustomerEmail());
-//                    }
-//                });
-//        return  customerList
-//                .stream()
-//                .filter(c-> c.getCustomerId()== customerId)
-//                .findFirst()
-//                .get();
         return customerDAO.save(customer);
     }
 
     public void deleteCustomer(int customerId) {
-//        customerList
-//                .stream()
-//                .forEach(c->{
-//                    if (c.getCustomerId()==customerId){
-//                        customerList.remove((c));
-//                    }
-//                });
         customerDAO.deleteById(customerId);
     }
 }
